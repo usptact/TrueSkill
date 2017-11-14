9f465374dd516c5732070b00384173ffe6641cab
Vladislavs Dovgalecs <vladislavsd@voicebox.com>
2017-11-14 09:51:59 -0800

    Rewriting model - declaring drawMargin as Gaussian random variable

--- TrueSkill/Program.cs
+++ TrueSkill/Program.cs
@@ -22,50 +22,41 @@ namespace TrueSkill
                                                 0}; // Draw
 
             int numGames = outcomes_data.Length;
-            Range n = new Range(numGames);
+            Range n = new Range(numGames).Named("n");
 
             // prior beliefs on skill
-            var Jskill = Variable.GaussianFromMeanAndVariance(120, 5 * 5);
-            var Fskill = Variable.GaussianFromMeanAndVariance(100, 40 * 40);
+            var Jskill = Variable.GaussianFromMeanAndVariance(120, 5 * 5).Named("JillSkill");
+            var Fskill = Variable.GaussianFromMeanAndVariance(100, 40 * 40).Named("FredSkill");
 
             // performance for 1+ games for Jill
-            var Jperfs = Variable.Array<double>(n);
+            var Jperfs = Variable.Array<double>(n).Named("JillPerfs");
             Jperfs[n] = Variable.GaussianFromMeanAndVariance(Jskill, 5 * 5).ForEach(n);
 
             // performance for 1+ games for Fred
-            var Fperfs = Variable.Array<double>(n);
+            var Fperfs = Variable.Array<double>(n).Named("FredPerfs");
             Fperfs[n] = Variable.GaussianFromMeanAndVariance(Fskill, 5 * 5).ForEach(n);
 
-            var drawMargin = Variable.GammaFromMeanAndVariance(1.0, 10.0);
+            var drawMargin = Variable.GaussianFromMeanAndVariance(1.0, 10.0).Named("drawMargin");
+            Variable.ConstrainPositive(drawMargin);
 
             // game outcomes (0 - Draw; 1 - Jill wins; 2 - Fred wins)
-            var outcomes = Variable.Array<int>(n);
+            var outcomes = Variable.Array<int>(n).Named("outcomes");
 
             // model
             using (Variable.ForEach(n))
             {
-                using (Variable.If(Jperfs[n] > Fperfs[n]))
-                {
-                    var JillWins = Jperfs[n] - Fperfs[n] - drawMargin;
-                    using (Variable.If(JillWins > 0))
-                        outcomes[n] = 1;
-                    using (Variable.IfNot(JillWins > 0))
-                        outcomes[n] = 0;
-                }
-                using (Variable.If(Fperfs[n] > Jperfs[n]))
-                {
-                    var FredWins = Fperfs[n] - Jperfs[n] - drawMargin;
-                    using (Variable.If(FredWins > 0))
-                        outcomes[n] = 2;
-                    using (Variable.IfNot(FredWins > 0))
-                        outcomes[n] = 0;
-                }
+                using (Variable.If(Jperfs[n] < Fperfs[n] + drawMargin | Jperfs[n] < Fperfs[n] + drawMargin))
+                    outcomes[n] = 0;
+                using (Variable.If(Jperfs[n] > Fperfs[n] + drawMargin))
+                    outcomes[n] = 1;
+                using (Variable.If(Fperfs[n] > Jperfs[n] + drawMargin))
+                    outcomes[n] = 2;
             }
 
             // attaching data
             outcomes.ObservedValue = outcomes_data;
 
-            InferenceEngine engine = new InferenceEngine(new VariationalMessagePassing());
+            InferenceEngine engine = new InferenceEngine();
 
             Gaussian JskillMarginal = engine.Infer<Gaussian>(Jskill);
             Gaussian FskillMarginal = engine.Infer<Gaussian>(Fskill);
